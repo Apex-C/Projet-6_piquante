d6a25db5be1faaee0d66e42956c371cd606b8dc0
Chedli HAMMAMI <c.hammami@live.fr>
2022-04-08 17:03:12 +0200

    remove frontend file

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -7,10 +7,7 @@ exports.createScauce = (req, res) => {
     let reghexChar = new RegExp(/[<>;$&"]/g);
     const sauce = new Sauce({
         ...sauceObject,
-        /**  likes: 0,
-          dislikes: 0,
-          usersLiked: [],
-          usersDisliked: [], */
+
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
 
